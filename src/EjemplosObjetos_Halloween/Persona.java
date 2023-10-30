e2627d581f73f60f238ac19e067673d1a1fe7601
developer <developer@505811fdadf7>
2023-10-30 12:02:55 +0000

    primer commit de Halloween con objetos

--- src/EjemplosObjetos_Halloween/Persona.java
+++ src/EjemplosObjetos_Halloween/Persona.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package EjemplosObjetos.Halloween;
+package EjemplosObjetos_Halloween;
 
 /**
  *
