db448d46c8dab82c516f9c24a577c982d439f861
developer <developer@505811fdadf7>
2023-11-07 11:28:33 +0000

    primer commit del juego con la ficha

---
+++ src/Ejercicio7_2/Ficha.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio7_2;
+
+/**
+ *
+ * @author developer
+ */
+public class Ficha {
+    private int posicion;
+
+    //Constructores
+    public Ficha(){
+        this.posicion= 0;
+    }
+    
+    public Ficha(int numero){
+        this.posicion= numero;
+    }
+    
+
+
+    
+    //getter y setter
+    
+}
