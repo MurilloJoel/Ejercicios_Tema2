f061a4b66055676d7a3a8822311941723ec7249f
developer <developer@505811fdadf7>
2023-11-07 11:27:43 +0000

    commit de cuenta para el ej6

--- src/Ejercicio2/Cuenta.java
+++ src/Ejercicio2/Cuenta.java
@@ -15,22 +15,45 @@ public class Cuenta {
     int numCuenta;
     
     //constructor
-    Cuenta(int cuenta, double inicial){
+    public Cuenta(int cuenta, double inicial){
         this.numCuenta=cuenta;
         this.saldo=inicial;
     }
     
     //metodos
-    double saldo(){
+    public double saldo(){
         return this.saldo;
     }
     
-    void depositar(double cantidad){
+    public void depositar(double cantidad){
         this.saldo+=cantidad;
     }
-    void retirar(double cantidad){
+    public void retirar(double cantidad){
         this.saldo-=cantidad;
     }
+    
+   /*  PRIMERA MANERA DEL EJERCICIO 6
+    * operando con los atributos    
+    */
+    
+    public void transferencia1 (Cuenta c2, double valor){
+        if(this.saldo >= valor ){
+            this.saldo -= valor;
+            c2.saldo += valor;
+        }
+    }
+    
+    /*  SEGUNDA MANERA DEL EJERCICIO 6
+    * operando con los metodos retirar y depositar    
+    */
+    
+    public void transferencia2 (Cuenta c2, double valor){
+        if(this.saldo >= valor ){
+            this.retirar(valor);
+            c2.depositar(valor);
+        }
+    }
+    
     @Override
     public String toString(){
         return ("La cuenta "+this.numCuenta + " tiene un saldo de: "+ this.saldo+ " €");
