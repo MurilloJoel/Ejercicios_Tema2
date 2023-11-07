2a1a9cbe7b33b974c8840f459fe7b4e85ee8405f
joel <joel@DESKTOP-BO0U0VT>
2023-11-07 18:22:27 +0100

    ultimo commit del juego de la oca

--- src/Ejercicio7_2/Ficha.java
+++ src/Ejercicio7_2/Ficha.java
@@ -10,6 +10,8 @@ package Ejercicio7_2;
  * @author developer
  */
 public class Ficha {
+
+    
     private int posicion;
 
     //Constructores
@@ -21,9 +23,33 @@ public class Ficha {
         this.posicion= numero;
     }
     
+    
+    //otros metodos
+    public void avanzar(){
+        this.posicion++;
+    }
+    
+    public void retroceder(){
+        this.posicion--;
+    }
+    
+    public void saltar(int num){
+        this.posicion += num;
+    }
 
+    @Override
+    public String toString() {
+        return "La ficha se encuentra en la posicion: "+ posicion;
+    }
 
     
     //getter y setter
+    public int getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
     
 }
--- src/Ejercicio7_2/Juego.java
+++ src/Ejercicio7_2/Juego.java
@@ -1,21 +1,67 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
+    Ejercicio 7
+
+    Realiza una clase Ficha que tenga un solo atributo de nombre posicion, que es del tipo entero.
+
+    Tendrá dos constructores:
+
+    Sin parámetros  que empieza en la posición 0
+    Con un parámetro que inicie la posición según el argumento recibido
+    Métodos:
+        avanzar() suma uno a la posición de la ficha.
+        retroceder() resta uno a la posición de la ficha.
+        saltar(int num) recibe un número de posiciones que se suman a la posición de la ficha.
+        toString() que devuelve información del objeto.
+    
+    Crea una clase controladora de nombre Juego con método main ya haz avanzar la ficha varias veces,
+    retroceder y saltar, sacando de cuando en cuando información de por donde va.
  */
 package Ejercicio7_2;
 
 /**
  *
- * @author developer
+ * @author joel
  */
 public class Juego {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       Ficha f1= new Ficha();
+       Ficha f2= new Ficha(10);
+       
+        System.out.println("-------------------------------------------------------------------------");
+        
+        System.out.println("                      FICHAS INICIALES");
+        
+        System.out.println("Ficha 1: " + f1.toString());
+        System.out.println("Ficha 2: " + f2.toString());
+        
+        System.out.println("-------------------------------------------------------------------------");
+        
+        
+        f1.avanzar();
+        f2.retroceder();
+        
+        System.out.println("-------------------------------------------------------------------------");
+        
+        System.out.println("                      FICHAS MODIFICADAS");
+        
+        System.out.println("Ficha 1 (avanzada): " + f1.toString());
+        System.out.println("Ficha 2 (retrocedida): " + f2.toString());
+        
+        System.out.println("-------------------------------------------------------------------------");
+        
+        
+        
+        f2.saltar(5);
+        
+        System.out.println("-------------------------------------------------------------------------");
+        
+        System.out.println("                      FICHA 2 ASIGNANDOLE UN VALOR");
+        
+        System.out.println("Ficha 2: " + f2.toString());
+        
+        System.out.println("-------------------------------------------------------------------------");
     }
     
 }
